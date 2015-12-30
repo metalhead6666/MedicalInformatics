886ec1483baad2224c0d5d2afb95459e9dd5bac9
metalhead_6666 <noe.godinho.92@gmail.com>
2015-12-30 18:56:33 +0000

    menu created

--- Assignment 4/JoaoTiagoFernandes_NoeGodinho.m
+++ Assignment 4/JoaoTiagoFernandes_NoeGodinho.m
@@ -1,10 +1,34 @@
 clear all;clc
+warning('off','all');
 
-warning('off','all')
+choice = 0;
 
-%load 'DATARR//DARR_003.mat'
-%load 'DATPVC//DPVC_228.mat'
-%ecg = DAT.ecg;
-ecg = load('ecgnoise.dat');
+while choice ~= 1 && choice ~= 2
+    choice = input('1-Analyze\n2-Test\n');
+end
 
-analyzeECG(ecg);
+if choice == 1
+    % files used to analyze the application
+    load 'DATARR//DARR_022.mat';
+    load 'DATARR//DARR_029.mat';
+    load 'DATPVC//DPVC_106.mat';
+    load 'DATPVC//DPVC_210.mat';
+else
+    % choose a file randomly to test the application
+    dirfiles = dir('DATARR');
+    array = dirfiles(3 : length(dirfiles)-1);
+    dirfiles = dir('DATPVC');
+    array = [array; dirfiles(3 : length(dirfiles)-1)];
+    randnum = randi(length(array));
+    filename = array(randnum).name;
+    
+    if strcmp(filename(1:4),'DPVC') == 1
+        str = strcat('DATPVC//', filename);
+    else
+        str = strcat('DATARR//', filename);
+    end
+    
+    load(str);
+end
+
+analyzeECG(DAT.ecg);
