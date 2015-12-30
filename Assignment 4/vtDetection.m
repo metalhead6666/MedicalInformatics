ab492bf453abc92137f665bb48c6ddb4f8841172
metalhead_6666 <noe.godinho.92@gmail.com>
2015-12-30 18:22:55 +0000

    VT detection working

--- Assignment 4/vtDetection.m
+++ Assignment 4/vtDetection.m
@@ -1,14 +1,21 @@
-function vtDetection(ECG, R)
+function isVT = vtDetection(ECG, R, seconds)
     % ArModel = ar(ECG, 4);
     % A = ArModel.A;
     % rootsAr = roots(A);
 
     % zplane(rootsAr);
     
-    [w, fs] = pwelch(ECG);
+    [w, ~] = pwelch(ECG);
 
     temp = w(1 : length(w)/8);
     plot(temp, 'r');
-    
     %plot(locs, w(locs), 'ro');
+    
+    heart_beats = length(R)/seconds;
+    
+    if heart_beats > 1
+        isVT = 1;
+    else
+        isVT = 0;
+    end  
 end
